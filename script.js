2e25ae56bc26e6a73c7fa596444be4d13ae5f1c3
skybreakbb <willistaylora@gmail.com>
2023-05-15 19:28:42 -0500

    added function to determine time as applied to divs (thanks anthony)

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var hour = dayjs().hour() -9;
+  console.log(hour);
+  $('.time-block').each(function() {
+    console.log($(this).attr('id'));
+    var id = $(this).attr('id').split('-')[1];
+    var rowHour = parseInt(id);
+
+    if (hour > rowHour) {
+      $(this).addClass('past');
+    } else if (hour < rowHour) {
+      $(this).addClass('future');
+    } else {
+      $(this).addClass('present');
+    }
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
