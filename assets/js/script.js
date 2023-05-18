036984fcc70540fa020e3b3e49560263c5b3a178
skybreakbb <willistaylora@gmail.com>
2023-05-18 15:13:06 -0500

    added button to clear all localStorage and refresh page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(function () {
   $('.time-block').each(function() {
     var id = $(this).attr('id').split('-')[1];
     var rowHour = parseInt(id);
-    (hour > rowHour) ? $(this).addClass('past') 
+    (hour > rowHour) ? $(this).addClass('past')
     : (hour < rowHour) ? $(this).addClass('future') 
     : $(this).addClass('present');
   });
@@ -16,6 +16,13 @@ $(function () {
     var inputParent = $(this).parent().attr("id");
     localStorage.setItem(inputParent, inputText);
   });
+
+  // click event to clear all localStorage and refresh browser
+  $(".clearBtn").on("click", function() {
+    localStorage.clear();
+    location.reload();
+  });
+
   // for loop to getItem from localStorage and populate textarea in html
 for (let i = 9; i < 18; i++) {
   var savedInput = (localStorage.getItem("hour-" + [i]));
